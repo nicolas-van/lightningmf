37a4ce87e1c34580755cf378e7b717dd00b5ec61
nicolas-van <niv@niv-hp5320m.(none)>
2012-07-13 15:40:03 +0200

    Attempt to add a shortcut

--- install/deb/mk.sh
+++ install/deb/mk.sh
@@ -15,6 +15,18 @@ mv ../../dist/$package-*.tar.gz .
 py2dsc $package-*.tar.gz
 cp control deb_dist/$package-*/debian/ || exit -1
 cp copyright deb_dist/$package-*/debian/
+
+pushd .
+cd deb_dist/$package-*/
+debuild
+popd
+
+pushd .
+cd deb_dist/$package-*/debian/$package/usr/share/
+mkdir applications
+popd
+cp *.desktop deb_dist/$package-*/debian/$package/usr/share/applications/
+
 pushd .
 cd deb_dist/$package-*/
 debuild
