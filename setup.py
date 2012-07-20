9561ece14e9db86cfbc61ba04ce100ea96fc735f
nicolas-van <niv@niv-hp5320m.(none)>
2012-07-20 14:06:27 +0200

    Set new version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 import os.path
 
 setup(name='lightningmf',
-      version='1.0.0',
+      version='1.0.1',
       description='Lightning MAME Frontend',
       author='Nicolas Vanhoren',
       author_email='nicolas.vanhoren@unknown.com',
